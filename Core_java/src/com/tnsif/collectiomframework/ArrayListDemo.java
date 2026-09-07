340865b71c2c8d34f9b4531e468191c23e14c206
tnsda <tnsda@TNS_DA02>
2026-09-07 11:45:19 +0530

    commit

---
+++ Core_java/src/com/tnsif/collectiomframework/ArrayListDemo.java
@@ -0,0 +1,19 @@
+package com.tnsif.collectiomframework;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		
+	ArrayList<String>p=new ArrayList<>();
+	p.add("Laptop");
+	p.add("mobile");
+	p.add("Headphone");
+	
+	System.out.println(p);
+	System.out.println("product 1:"+p.get(1));
+
+	}
+
+}
