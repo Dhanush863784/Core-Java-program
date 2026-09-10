ed4b618d660a61ebbea0b9c9d53d7aef6089d4dd
Dhanush <schrodinger277363@gmail.com>
2026-09-10 11:36:23 +0530

    Added lambda expression programs

---
+++ Core_java/src/com/tnsif/lambdaexp/MultipleStatements.java
@@ -0,0 +1,5 @@
+package com.tnsif.lambdaexp;
+
+public class MultipleStatements {
+
+}
---
+++ Core_java/src/com/tnsif/lambdaexp/WithLambda.java
@@ -0,0 +1,10 @@
+package com.tnsif.lambdaexp;
+
+public class WithLambda {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
